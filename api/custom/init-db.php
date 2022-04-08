d83741997b08657dd2e95a04e17d56f8b11c9425
frimpongOpoku <pongofrimi@gmail.com>
2022-04-08 04:59:10 +0400

    init-db also tweaked a bit

--- api/custom/init-db.php
+++ api/custom/init-db.php
@@ -7,15 +7,16 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "frimpongDB";
+$dbname = "noGamesMyGee";
 
 // ------------------- CREATE THE DB ITSELF ------------------
 try {
+  echo'<div style="background:#faf1e3; height:100%; width:100%; display:flex; flex-direction:column; align-items:center; justify-content:center;">';
   $conn = new PDO("mysql:host=$servername", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "CREATE DATABASE $dbname";
   $conn->exec($sql);
-  echo "Database created successfully<br>";
+  echo "<h1>Your database was created successfully!</h1>";
 } catch (PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
@@ -27,12 +28,14 @@ $tablename = "messaging";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "'$tablename' was created Successfully! <br />";
+  echo "<h2>The <i>'$tablename'</i> table was also created Successfully!</h2>";
 } catch (PDOException $e) {
   echo "Sorry, we could not create the table '$tablename' -> " . $e->getMessage();
 }
 $query = $conn->prepare("CREATE TABLE $tablename (id int(6) NOT NULL auto_increment, email varchar(30) NOT NULL,msg varchar(500) NOT NULL,sub varchar(500) NOT NULL,PRIMARY KEY (id),UNIQUE id (id),KEY id_2 (id))");
 $query->execute();
 
+
+echo '</div>'
 
 ?>
